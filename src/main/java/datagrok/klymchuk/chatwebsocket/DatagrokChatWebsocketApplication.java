bba3b507ab8d37ba6c8b761acda60f6a9815717d
Klimchuk_AM <qqvalarqq@gmail.com>
2023-01-17 12:13:41 +0200

    initial commit

---
+++ src/main/java/datagrok/klymchuk/chatwebsocket/DatagrokChatWebsocketApplication.java
@@ -0,0 +1,13 @@
+package datagrok.klymchuk.chatwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatagrokChatWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatagrokChatWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/datagrok/klymchuk/chatwebsocket/DatagrokChatWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package datagrok.klymchuk.chatwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatagrokChatWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
